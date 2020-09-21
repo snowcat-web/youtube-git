8172803fb7bf22e6de2012b249a520f30e94585c
Donovan Rincon <rincondonovan@gmail.com>
2020-09-21 15:16:44 -0500

    fix search

--- src/App.js
+++ src/App.js
@@ -1,57 +1,38 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-import { render } from "react-dom";
-import Toolbar from "./components/Toolbar/Toolbar";
-import SideNav from "./components/SideNav/sideNav";
-import Backdrop from "./components/Backdrop/Backdrop";
 import YTSearch from "./components/YoutubeSearch";
-import VideoList from "./components/Video/VideoList";
 import VideoBody from "./components/Video/VideoBody";
 import Navigation from "./components/Navigation";
-import { Route } from 'react-router';
 
-import Favorites from "./components/Favorites/Favorites";
-//const API_KEY = process.env.REACT_APP_API_KEY;
 const API_KEY = "AIzaSyB-YiVwk4DoZ-K4NjLcayTWYG7OGZZ9DxE";
 
-const App = () => { 
-  const [videos, setVideos] = useState([]);
-  const [sldVideo, setSldVideo] = useState(null);
+const App = () => {
+    const [videos, setVideos] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
 
-  useEffect(() => {
-    fetchResource("kittens");
-  }, []);
+    useEffect(() => {
+        fetchResource("kittens");
+    }, []);
 
-  const useShareableState = () => {
-    const [username, setUsername] = useState('Abrar');
-    const [count, setCount] = useState(0);
-    return {
-      username,
-      setUsername,
-      count,
-      setCount
-    }
-  }
+    const fetchResource = async (item) => {
+        await YTSearch(
+            { key: API_KEY, term: item, params: { maxResults: 20 } },
+            (data) => {
+                setVideos(data);
+            }
+        );
+    };
 
-  const fetchResource = async (item) => {
-    await YTSearch(
-      { key: API_KEY, term: item, params: { maxResults: 20 } },
-      (data) => {
-        console.log(data);
-        setVideos(data);
-      }
-    );
-  };
-
-  return (
+    const handleSearchChange = async(value) => {
+        await fetchResource(value)
+    };
+    return (
     <div>      
-      <Navigation />
+      <Navigation onSearchChange={handleSearchChange}/>
       <div className="content">        
-        <VideoBody
-        onVideoSelect={selected => setSldVideo(selected)}
-        videos={videos}
-        videoSelected = {sldVideo}
-        />
+        <VideoBody onVideoSelect={selected => setSelectedVideo(selected)}
+                                videos={videos}
+                                videoSelected = {selectedVideo} />
       </div>
     </div>
   );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,51 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Toolbar from "./Toolbar/Toolbar";
 import SideNav from "./SideNav/sideNav";
 import Backdrop from "./Backdrop/Backdrop";
-import YTSearch from "./YoutubeSearch";
 
-//const API_KEY = process.env.REACT_APP_API_KEY;
-const API_KEY = "AIzaSyB-YiVwk4DoZ-K4NjLcayTWYG7OGZZ9DxE";
-
-const Navigation = () => {
+const Navigation = (props) => {
   const [sideNavOpen, setSideNavOpen] = useState(false);
-  const [videos, setVideos] = useState([]);
-  const [sldVideo, setSldVideo] = useState(null);
 
-  useEffect(() => {
-    fetchResource("kittens");
-  }, []);
-
-  const fetchResource = async (item) => {
-    await YTSearch(
-      { key: API_KEY, term: item, params: { maxResults: 20 } },
-      (data) => {
-        console.log(data);
-        setVideos(data);
-      }
-    );
-  };
-
-   var sideNavOpenHandler = () => {
+   const sideNavOpenHandler = () => {
      setSideNavOpen(!sideNavOpen);
      return false;
    };
 
-   var sideNavCloseHandler = () => {
+   const sideNavCloseHandler = () => {
      setSideNavOpen(false);
    };
 
-  var backdrop = null;
+  let backdrop = null;
 
   if (sideNavOpen) {
-     //sideNav = <SideNav />;
      backdrop = <Backdrop click={sideNavCloseHandler} />;
   }
 
   return (
     <div>
       <Toolbar sideNavOpenHandler={sideNavOpenHandler}
-      onSearchTermChange={searchTerm => fetchResource(searchTerm)}  />
+      onSearchChange={props.onSearchChange}  />
       <SideNav show={sideNavOpen} />
       {backdrop}
       </div>    
